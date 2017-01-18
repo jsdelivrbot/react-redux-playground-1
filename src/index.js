c57b1aa12a8e11d71a0667f0d243d83ef1d74f63
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2017-01-19 00:48:19 +0700

    Revert to base react

--- src/index.js
+++ src/index.js
@@ -1,52 +1,15 @@
-import _ from 'lodash';
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-import SearchBar from './components/search/bar';
-import VideoList from './components/video/list';
-import VideoDetails from './components/video/details';
+import App from './components/app';
+import reducers from './reducers';
 
-const API_KEY = 'AIzaSyARmfFeLZDSacozmqlxcvoonAG7ztKsfpY';
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            videos: [],
-            selectedVideo: null
-        };
-
-        this.videoSearch('cats');
-    }
-
-    videoSearch = (term) => {
-        YTSearch({ key: API_KEY, term: term }, (videos) => {
-            this.setState({
-                videos: videos,
-                selectedVideo: videos[0]
-            })
-        });
-    };
-
-    render() {
-        const videoSearch = _.debounce((term) => {
-            this.videoSearch(term)
-        }, 300);
-
-        return (
-            <div>
-                <SearchBar onSearchTermChange={videoSearch} />
-                <div className="row">
-                    <VideoDetails video={this.state.selectedVideo} />
-                    <VideoList
-                        onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
-                        videos={this.state.videos} />
-                </div>
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
